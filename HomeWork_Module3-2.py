e68285795cfc43aa06c00455fe59b59cbeb48854
Okanarchuk <152897435+Okanarchuk@users.noreply.github.com>
2024-03-13 15:06:56 +0100

    Add files via upload

--- HomeWork_Module3-2.py
+++ HomeWork_Module3-2.py
@@ -4,14 +4,20 @@ def get_numbers_ticket(min, max, quantity):
     try:
         if min < quantity < max + 1:
             return "Quantity should not be less or greater than the range."
+        if min <= 1 or max <= 1000:
+            return "Minumum should be greater than 1. Maximum should not exceed 1000."
+        
         unique_numbers = set()
         numbers = list(range(min, max + 1))
+        if quantity > len(numbers):
+            return "Quantity should not exceed the range between Min and Max."  
+
+        unique_numbers = random.sample(numbers, quantity)
+        unique_numbers.sort()
+        return unique_numbers
+    
     except TypeError:
         return "Invalid numbers input"
-    
-    unique_numbers = random.sample(numbers, quantity)
-    unique_numbers.sort()
-    return unique_numbers
 
-lottery_numbers = get_numbers_ticket(4, 49, 6)
+lottery_numbers = get_numbers_ticket(4, 49, 600)
 print("Ваші лотерейні числа:", lottery_numbers)
